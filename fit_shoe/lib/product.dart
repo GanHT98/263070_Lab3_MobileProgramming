a004d5d7c97c8c0935fca2712c0c10e28247353c
GanHT98 <51353902+GanHT98@users.noreply.github.com>
2020-06-25 20:47:05 +0800

    Lab3

---
+++ fit_shoe/lib/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  String pid, name, price, quantity, weigth, gender, type, date;
+  Product({
+    this.pid,
+    this.name,
+    this.price,
+    this.quantity,
+    this.weigth,
+    this.gender,
+    this.type,
+    this.date,
+  });
+}
